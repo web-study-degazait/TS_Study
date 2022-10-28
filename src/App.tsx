5c71e361423333dc797c4df6b1d43b5011317140
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-10-28 14:34:03 +0900

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+  //theme에서 bgColor을 가져옴
+`;
+const H1 = styled.div`
+  color: ${(props) => props.theme.textColor};
+`;
+
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello ", value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Air Pod 2</H1>
+    </Container>
   );
 }
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { DarkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={DarkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
